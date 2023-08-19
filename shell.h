43ac31b52b28224fb3123e7aae2bc43b7eaf642d
JOlindi <olindijosphat75@gmail.com>
2023-08-19 12:55:34 -0700

    Shell

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL
+#define SHELL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <ctype.h>
+
+
+#endif
